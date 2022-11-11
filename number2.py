77432790f7f2ec81bcdf1cd9968ed656e2ddcefa
jhasudesu <72380350+jchiros@users.noreply.github.com>
2022-11-11 17:14:58 +0800

    Add files via upload

--- number2.py
+++ number2.py
@@ -5,7 +5,31 @@ num = input("Enter a number: ")
 
 
 list_num = list(num)
-reversed_list = list_num[::-1]
-print("Reverse: ", end =''.join(reversed_list))
+reversed_list = list_num
+
+if list_num[-1] == '0':
+    new_list = reversed_list[:-1]
+    reversed_list = new_list
+    print("Reverse: ", end=''.join(reversed_list[::-1]))
+
+elif '-' in list_num:
+    if list_num[-1] == '0':
+        remove_zero = reversed_list[:-1]
+        retain_sign = remove_zero.insert(0,'-')
+        print("Reverse: ", end=''.join(retain_sign))
+    else:
+        remove = list_num[1:]
+        new_list = remove[::-1]
+        new_list.insert(0, "-")
+        print("Reverse: ", end=''.join(new_list))
+
+elif len(reversed_list) == 1:
+    reversed_list.append('0')
+    print("Reverse: ", end=''.join(reversed_list))
+else:
+    reversed_list = list_num[::-1]
+    print("Reverse: ", end=''.join(reversed_list))
+
+
 
 
--- number3.py
+++ number3.py
@@ -6,9 +6,9 @@ txt_list = []
 i = 0
 for i in range(len(text)):
     if text[i] not in txt_list:
-        txt_list.insert(i, text[i])
+        txt_list.insert(i, text[i].lower())
         i += 1
 
 text = ""
 text = text.join(txt_list)
-print("New String:", text)
+print("New String:", text.title())
